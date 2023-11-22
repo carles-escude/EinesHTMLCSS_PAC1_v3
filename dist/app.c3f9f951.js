3f066a1224d5cb59d6e1dd0084345252c591892d
carles-escude <75261579+carles-escude@users.noreply.github.com>
2023-11-22 21:31:25 +0100

    wip

--- dist/app.c3f9f951.js
+++ dist/app.c3f9f951.js
@@ -201,7 +201,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50938" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51072" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
@@ -346,4 +346,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../../../Users/DELL OPTIPLEX 3050/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/app.js"], null)
-//# sourceMappingURL=/app.c3f9f951.js.map
+//# sourceMappingURL=app.c3f9f951.js.map
